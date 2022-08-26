e5ee602c227592ff42b694c5cfd389fb9dfbd2fb
NadezhdaDoneva <donevanad@gmail.com>
2022-08-26 20:18:07 +0300

    change

--- DOM-Manipulation-And-Events/07. Shopping-Cart/solution.js
+++ DOM-Manipulation-And-Events/07. Shopping-Cart/solution.js
@@ -1,10 +1,3 @@
 function solve() {
-   let buttonElements = Array.from(document.querySelectorAll('button[class="add-product"]'));
-   buttonElements.forEach(el => {
-      el.addEventListener('click', onButtonClick)
-   });
-   function onButtonClick(e){
-      let divElement = e.currentTarget.parentElement.parentElement;
-      console.log(divElement);
-   }
+   //to do...
 }
